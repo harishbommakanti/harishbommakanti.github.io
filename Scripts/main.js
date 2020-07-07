bb4d77521d308423f015aad8a11d28e74176f637
Harish <harishbommakanti2002@gmail.com>
2020-07-07 16:34:54 -0500

    added vuepress into this project

---
+++ Scripts/main.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+    var head = new Vue({
+        el: '#head',
+        data: {
+            htmlcontent:   `   <meta charset="UTF-8">
+                            <meta name='viewport' content="width=device-width, initial-scale=1.0">
+        
+                            <script src = "Scripts/vue.js"></script>
+                            <script src = "Scripts/main.js"></script>
+        
+                            <title>Portfolio - Harish Bommakanti</title>
+                        `
+        } 
+    });
+    var App = new Vue({
+
+    });
+}
